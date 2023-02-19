9c4e60a5e244740e5efb94606333385fffc9e4fd
Молодец 2705 <ant_papa@mail.ru>
2023-02-19 13:49:33 +0600

    second test

--- src/01-hello-tests/01.test.tsx
+++ src/01-hello-tests/01.test.tsx
@@ -1,4 +1,4 @@
-import {sum} from "./01";
+import {splitIntoWords, sum} from "./01";
 import {mult} from "./01";
 
 
@@ -25,5 +25,20 @@ test('multiply should be correct', () => {
 
     expect(result1).toBe(2);
     expect(result2).toBe(6);
+
+})
+
+test('splitting into words should be correct', () => {
+    const sent1 = "Hello my friend!";
+    const sent2 = "JS - the best programming language.";
+
+    const result1 = splitIntoWords(sent1);
+    const result2 = splitIntoWords(sent2);
+
+    expect(result1.length).toBe(3);
+    expect(result1[0]).toBe("hello");
+    expect(result1[1]).toBe("my");
+    expect(result1[2]).toBe("friend");
+    expect(result2.length).toBe(5);
 
 })
