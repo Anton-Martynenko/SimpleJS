ce388b8e11f42bc458cd2d7e1c1c9f30d3c971d7
Молодец 2705 <ant_papa@mail.ru>
2023-02-19 14:33:31 +0600

    before Each

--- src/01-hello-tests/01.test.tsx
+++ src/01-hello-tests/01.test.tsx
@@ -1,24 +1,28 @@
 import {splitIntoWords, sum} from "./01";
 import {mult} from "./01";
 
+let a: number;
+let b: number;
+let c: number;
+
+beforeEach( () => {
+    a = 1;
+    b = 2;
+    c = 3;
+})
 
 test('sum should be correct', () => {
-    const a = 1;
-    const b = 2;
-    const c = 3;
 
     const result1 = sum(a, b);
+    b = 100;
     const result2 = sum(b, c);
 
     expect(result1).toBe(3);
-    expect(result2).toBe(5);
+    expect(result2).toBe(103);
 
 })
 
 test('multiply should be correct', () => {
-    const a = 1;
-    const b = 2;
-    const c = 3;
 
     const result1 = mult(a, b);
     const result2 = mult(b, c);
