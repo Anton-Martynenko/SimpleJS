9137865eee6d7a6f6be971d9d90aeb0b179c20b5
Молодец 2705 <ant_papa@mail.ru>
2023-02-20 09:48:17 +0600

    student type

--- src/02/02.tsx
+++ src/02/02.tsx
@@ -1,5 +1,55 @@
+type StudentTechnologiesType = {
+    id: number
+    title: string
+}
+
+type StudentCityType = {
+    title: string
+    countryTitle: string
+}
+
+type StudentAdressType = {
+    streetTitle: string
+    city: StudentCityType
+}
+
+type StudentType = {
+    id: number
+    name: string
+    age: number
+    isActive: boolean
+    adress: StudentAdressType
+    technologies: Array<StudentTechnologiesType>
+}
+
 export const student = {
     id: 1,
     name: "Dimych",
-    age: 32
+    age: 32,
+    isActive: false,
+    adress: {
+        streetTitle: "Surganova 2",
+        city: {
+            title: "Minsk",
+            countryTitle: "Belarus"
+        }
+    },
+    technologies: [
+        {
+            id: 1,
+            title: "HTML"
+        },
+        {
+            id: 2,
+            title: "CSS"
+        },
+        {
+            id: 3,
+            title: "JS"
+        },
+        {
+            id: 4,
+            title: "React"
+        }
+    ]
 }
