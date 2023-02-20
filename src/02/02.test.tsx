744835fdc555d28b9442a9a01b87bf8a88d1ea5a
Молодец 2705 <ant_papa@mail.ru>
2023-02-20 13:52:19 +0600

    Houses

--- src/02/02.test.tsx
+++ src/02/02.test.tsx
@@ -2,10 +2,21 @@ import {CityType} from "./02_02";
 
 let city: CityType;
 
-beforeEach( () => {
+beforeEach(() => {
     city = {
         title: "New York",
-        houses: [],
+        houses: [{
+            buildedAt: 2012, repaired: false,
+            address: {number: 100, street: {title: "White street"}}
+        },
+            {
+                buildedAt: 2008, repaired: false,
+                address: {number: 100, street: {title: "Happy street"}}
+            },
+            {
+                buildedAt: 2020, repaired: false,
+                address: {number: 101, street: {title: "Happy street"}}
+            }],
         governmentBuildings: [],
         citizensNumber: 1000000
     }
