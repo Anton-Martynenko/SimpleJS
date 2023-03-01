7cd20b6d5101618de88e520a34eae52af010d114
Молодец 2705 <ant_papa@mail.ru>
2023-03-01 11:47:23 +0600

    courses

--- src/04/04.tsx
+++ src/04/04.tsx
@@ -3,3 +3,18 @@ export const ages = [18, 20, 22, 1 ,100, 90, 14];
 const predicate = (age: number) => {
   return age > 90;
 }
+
+type CourseType = {
+  title: string
+  price: number
+}
+
+const courses = [
+  {title: "CSS", price: 110},
+  {title: "JS", price: 200},
+  {title: "REACT", price: 150}
+]
+
+const chipPredicate = (course: CourseType) => {
+  return course.price < 160;
+}
