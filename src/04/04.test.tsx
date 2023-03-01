456c89dd049b3112ca8a337d99062bce88a61f93
Молодец 2705 <ant_papa@mail.ru>
2023-03-01 12:50:09 +0600

    true oldAges

--- src/04/04.test.tsx
+++ src/04/04.test.tsx
@@ -4,11 +4,7 @@ import {CourseType} from "./04";
 test("should take old men older then 90", () => {
     const ages = [18, 20, 22, 1 ,100, 90, 14];
 
-    const predicate = (age: number) => {
-        return age > 90;
-    }
-
-    const oldAges = ages.filter(predicate);
+    const oldAges = ages.filter(age => age > 90);
 
     expect(oldAges.length).toBe(1);
     expect(oldAges[0]).toBe(100);
