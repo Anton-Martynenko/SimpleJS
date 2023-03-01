d80d6c7675dad1060619722802f446bb2c4f9f23
Молодец 2705 <ant_papa@mail.ru>
2023-03-01 11:34:28 +0600

    04 test starts

---
+++ src/04/04.test.tsx
@@ -0,0 +1,14 @@
+import {ages} from "./04";
+
+test("should take old men older then 90", () => {
+    const ages = [18, 20, 22, 1 ,100, 90, 14];
+
+    const predicate = (age: number) => {
+        return age > 90;
+    }
+
+    const oldAges = ages.filter(predicate);
+
+    expect(oldAges.length).toBe(2);
+    expect(oldAges[0]).toBe(100);
+})
