a74c956447f74662eaff740fa4140904902b54a4
Молодец 2705 <ant_papa@mail.ru>
2023-03-01 14:05:50 +0600

    4_02 test

--- src/04/04_02.test.tsx
+++ src/04/04_02.test.tsx
@@ -1,6 +1,7 @@
 import {CityType} from "../02/02_02";
 import {addMoneyToBudget, createMessage, repairHouse, toFireStaff} from "../03/03";
 import {toHireStaff} from "../03/03";
+import {demolishHousesOnTheStreet} from "./04_02";
 
 let city: CityType;
 
@@ -8,29 +9,36 @@ beforeEach(() => {
     city = {
         title: "New York",
         houses: [{
-            buildedAt: 2012, repaired: false,
+            id: 1, buildedAt: 2012, repaired: false,
             address: {number: 100, street: {title: "White street"}}
         },
             {
-                buildedAt: 2008, repaired: false,
+                id: 2, buildedAt: 2008, repaired: false,
                 address: {number: 100, street: {title: "Happy street"}}
             },
             {
-                buildedAt: 2020, repaired: false,
+                id: 3, buildedAt: 2020, repaired: false,
                 address: {number: 101, street: {title: "Happy street"}}
             }],
         governmentBuildings: [
             {
                 type: "HOSPITAL", budget: 200000, staffCount: 200,
-                address: { street: { title: "Central Str" } }
+                address: {street: {title: "Central Str"}}
             },
             {
                 type: "FIRE-STATION", budget: 500000, staffCount: 1000,
-                address: { street: { title: "South Str" } }
+                address: {street: {title: "South Str"}}
             },
         ],
         citizensNumber: 1000000
     }
+})
+
+test("Houses shouid be destroyed", () => {
+    demolishHousesOnTheStreet(city, "Happy street");
+
+    expect(city.houses.length).toBe(1);
+    expect(city.houses[0].id).toBe(1);
 })
 
 test("Budget should be changed for HOSPITAL", () => {
