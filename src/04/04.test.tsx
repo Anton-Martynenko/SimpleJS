8a7374ba3a8d46adf6c78d105cda994d51ff8267
Молодец 2705 <ant_papa@mail.ru>
2023-03-01 11:40:45 +0600

    test 4.2

--- src/04/04.test.tsx
+++ src/04/04.test.tsx
@@ -9,6 +9,19 @@ test("should take old men older then 90", () => {
 
     const oldAges = ages.filter(predicate);
 
-    expect(oldAges.length).toBe(2);
+    expect(oldAges.length).toBe(1);
+    expect(oldAges[0]).toBe(100);
+})
+
+test("should take courses chipper 160", () => {
+    const ages = [18, 20, 22, 1 ,100, 90, 14];
+
+    const predicate = (age: number) => {
+        return age > 90;
+    }
+
+    const oldAges = ages.filter(predicate);
+
+    expect(oldAges.length).toBe(1);
     expect(oldAges[0]).toBe(100);
 })
