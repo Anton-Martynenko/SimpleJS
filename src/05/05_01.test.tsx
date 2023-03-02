6aaddae83314c5c606ba65836dcbebb5acb5719e
Молодец 2705 <ant_papa@mail.ru>
2023-03-02 15:57:25 +0600

    test 5

---
+++ src/05/05_01.test.tsx
@@ -0,0 +1,18 @@
+import {ManType} from "./05";
+
+let people: Array<ManType> = [];
+
+beforeEach(() => {
+    people = [
+        {name: "Andrew Ivanov", age: 33},
+        {name: "Alexander Petrov", age: 24},
+        {name: "Dmitry Sidorov", age: 18}
+    ]
+})
+
+test("should get array of greeting messages", () => {
+    const messages = people.map( man => `Hello ${man.name.split(' ')[0]}. Welcome to IT-Incubator!`);
+
+    expect(messages.length).toBe(2);
+    expect(messages[0]).toBe("Hello Andrew. Welcome to IT-Incubator!");
+})
