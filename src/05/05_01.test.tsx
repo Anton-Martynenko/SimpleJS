2760d841b186f54f900e8b67815f821539a252ed
Молодец 2705 <ant_papa@mail.ru>
2023-03-02 16:45:45 +0600

    greeting message

--- src/05/05_01.test.tsx
+++ src/05/05_01.test.tsx
@@ -1,4 +1,4 @@
-import {ManType} from "./05";
+import {createGreetingMessage, ManType} from "./05";
 
 let people: Array<ManType> = [];
 
@@ -13,6 +13,17 @@ beforeEach(() => {
 test("should get array of greeting messages", () => {
     const messages = people.map( man => `Hello ${man.name.split(' ')[0]}. Welcome to IT-Incubator!`);
 
-    expect(messages.length).toBe(2);
+    expect(messages.length).toBe(3);
     expect(messages[0]).toBe("Hello Andrew. Welcome to IT-Incubator!");
+    expect(messages[1]).toBe("Hello Alexander. Welcome to IT-Incubator!");
+    expect(messages[2]).toBe("Hello Dmitry. Welcome to IT-Incubator!");
+})
+
+test("should get array of greeting messages", () => {
+    const messages = createGreetingMessage(people);
+
+    expect(messages.length).toBe(3);
+    expect(messages[0]).toBe("Hello Andrew. Welcome to IT-Incubator!");
+    expect(messages[1]).toBe("Hello Alexander. Welcome to IT-Incubator!");
+    expect(messages[2]).toBe("Hello Dmitry. Welcome to IT-Incubator!");
 })
