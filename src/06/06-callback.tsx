c257e7a407f9e4c0ac9065dfeabfaa3b2813f9b4
Молодец 2705 <ant_papa@mail.ru>
2023-03-04 12:43:59 +0600

    save User

--- src/06/06-callback.tsx
+++ src/06/06-callback.tsx
@@ -12,8 +12,13 @@ const User = () => {
     const deleteUser = () => {
         alert ("user have been deleted")
     }
+
+    const saveUser = () => {
+        alert ("user have been saved")
+    }
+
     return <div>Dimych
         <button onClick={deleteUser}>delete</button>
-        <button>save</button>
+        <button onClick={saveUser}>save</button>
     </div>
 }
