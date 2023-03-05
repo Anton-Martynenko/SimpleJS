182ee2eac46b515e323627b68f14164fae4c33c3
Молодец 2705 <ant_papa@mail.ru>
2023-03-05 21:30:05 +0600

    title & man

--- src/06-destructuring/Destructuring.tsx
+++ src/06-destructuring/Destructuring.tsx
@@ -1,8 +1,29 @@
-type PropsType = {
+import React from 'react'
 
+export type ManType = {
+    name: string
+    age: number
+    lessons: Array<{title: string}>
+    address: {
+        street: {
+            title: string
+        }
+    }
+}
+
+type PropsType = {
+    title: string
+    man: ManType
 }
 
 export const ManComponent: React.FC<PropsType> = (props) => {
-
-    return <div></div>
+    const {title} = props;
+    const {name} = props.man;
+    return <div>
+        <h1>{props.title}</h1>
+        <hr/>
+        <div>
+            {props.man.name}
+        </div>
+    </div>
 }
