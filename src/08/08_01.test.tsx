523bca3102afb7a4600787b457267ffd06fd565e
Молодец 2705 <ant_papa@mail.ru>
2023-03-06 14:51:55 +0600

    08_01

---
+++ src/08/08_01.test.tsx
@@ -0,0 +1,11 @@
+import {users} from "./08_01";
+
+beforeEach(() => {
+
+})
+test("", () => {
+
+    })
+
+
+
---
+++ src/08/08_01.tsx
@@ -0,0 +1,15 @@
+export const usersArray = ["Dimych", "Natasha", "Valera", "Katya"]
+
+export const usersObj = {
+    '0': 'Dimych',
+    '1': 'Natasha',
+    '2': 'Valera',
+    '3': 'Katya'
+}
+
+export const users = {
+    '0': 'Dimych',
+    '1': 'Natasha',
+    '2': 'Valera',
+    '3': 'Katya'
+}
