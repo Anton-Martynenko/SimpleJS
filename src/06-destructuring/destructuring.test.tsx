a07823d559a7926f4d67bcd5a7748aa5017d811d
Молодец 2705 <ant_papa@mail.ru>
2023-03-06 14:10:10 +0600

    toStrictEqual

--- src/06-destructuring/destructuring.test.tsx
+++ src/06-destructuring/destructuring.test.tsx
@@ -2,6 +2,7 @@ import {CityType} from "../02/02_02";
 
 type LessonType = {
     title: string
+    name?: string
 }
 
 type ManType = {
@@ -20,7 +21,7 @@ beforeEach(() => {
     props = {
         name: "Dimych",
         age: 32,
-        lessons: [{title: "1"}, {title: "2"}, {title: "3"}],
+        lessons: [{title: "1"}, {title: "2"}, {title: "3", name: "react"}],
         address: {
             street: {
                 title: "Nezavisimosti street"
@@ -62,14 +63,17 @@ test('', () => {
     const l2 = props.lessons[1];
 
     //const [,, ls3] = props.lessons;
-    const [ls1, ...restLessons] = props.lessons;
+    const [,ls2, ...restLessons] = props.lessons;
 
     expect(l1.title).toBe('1');
     expect(l2.title).toBe('2');
 
     //expect(ls1.title).toBe('1');
     //expect(ls2.title).toBe('2');
-    expect(ls1.title).toBe('1');
-    expect(restLessons[0].title).toBe('2');
+    expect(ls2.title).toBe('2');
+    expect(restLessons.length).toBe(1);
+    expect(restLessons[0].title).toBe('3');
+    expect(restLessons[0].name).toBe("react");
+    expect(restLessons[0]).toStrictEqual({name: "react", title: "3"});
 })
 
