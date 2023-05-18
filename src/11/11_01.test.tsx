ce406937ef464d630663ee6e0568840e0a3b1aa2
Молодец 2705 <ant_papa@mail.ru>
2023-05-18 19:42:08 +0600

    test 11-1

--- src/11/11_01.test.tsx
+++ src/11/11_01.test.tsx
@@ -4,6 +4,10 @@ import {repeatWord} from "./11_01";
 // beforeEach(() => {
 //
 // })
-
+test('test 1', () => {
+    expect(repeatWord('w', 2, " ")).toBe('w w');
+    expect(repeatWord('yo', 5, ",")).toBe('yo,yo,yo,yo,yo');
+    expect(repeatWord(' ', 2, "!")).toBe(' ! ');
+})
 
 
